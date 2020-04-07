e39650fa99e4b4d63a056009a3221627bf36c5f4
hjleesm <hjleesm@naver.com>
2020-04-08 00:30:59 +0900

    Circular Progress Bar 구현

---
+++ Assets/Scripts/RadialProgress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialProgress : MonoBehaviour
+{
+    public Text ProgressIndicator;
+    public Image LoadingBar;
+    float currentValue;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentValue < 100)
+        {
+            currentValue += speed * Time.deltaTime;
+            ProgressIndicator.text = ((int)currentValue).ToString() + "%";
+        }
+        else
+        {
+            ProgressIndicator.text = "Done";
+        }
+
+        LoadingBar.fillAmount = currentValue / 100;
+    }
+}
